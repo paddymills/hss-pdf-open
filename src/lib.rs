20f8e5b1dc16b714e0c1da2fb12b1f550e1585a5
paddymills <paddymills@pm.me>
2020-09-02 09:51:57 -0400

    split into library and bin

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+use winreg::enums::HKEY_CLASSES_ROOT;
+use winreg::RegKey;
+
+fn get_pdf_handler() -> std::io::Result<String> {
+  let hkcr = RegKey::predef(HKEY_CLASSES_ROOT);
+
+  // get pdf handler
+  let pdf: String = hkcr.open_subkey(".pdf")?.get_value("")?;
+
+  // get location of pdf handler
+  let exec_path: PathBuf = [pdf.as_str(), "shell", "Open", "Command"].iter().collect();
+  let exec: String = hkcr.open_subkey(exec_path)?.get_value("")?;
+
+  // parse out exe only (may have shell argument placeholders in value)
+  let exe_pattern = Regex::new(r"C:[\w \\]+\.exe").unwrap();
+  let mat = exe_pattern.find(&exec).unwrap();
+  let exe = String::from(&exec[mat.start()..mat.end()]);
+
+  Ok(exe)
+}
